7246c705752b36f9e3a9e37e3db167885596ee98
Tormod <tormod_tho@hotmail.com>
2022-10-16 14:24:06 +0200

    jk

--- backend/build/cargo.sh
+++ backend/build/cargo.sh
@@ -2,6 +2,6 @@
 
 cargo install cargo-binstall
 
-cargo binstall cargo-edit cargo-expand cargo-outdated \
+cargo binstall -y cargo-edit cargo-expand cargo-outdated \
     cargo-watch cargo-geiger cargo-chef cargo-spellcheck \
     cargo-about cargo-valgrind
