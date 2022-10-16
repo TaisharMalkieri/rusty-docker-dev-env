94608d86ab2695d4f2526ca4ebd866a1e68bd2ca
Tormod <tormod_tho@hotmail.com>
2022-10-16 15:13:56 +0200

    aa

--- backend/build/cargo.sh
+++ backend/build/cargo.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-cargo install cargo-binstall
+
+cargo install cargo-binstall --bin
 
 cargo binstall -y cargo-edit cargo-expand cargo-outdated \
     cargo-watch cargo-geiger cargo-chef cargo-spellcheck \
