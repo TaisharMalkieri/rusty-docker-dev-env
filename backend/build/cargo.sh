812a44814be9d0c4933a3135cbc94bfed3cf4a31
Tormod <tormod_tho@hotmail.com>
2022-10-17 14:14:11 +0200

    ss

--- backend/build/cargo.sh
+++ backend/build/cargo.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-
 cargo install cargo-binstall --bin
+cargo binstall cargo-binstall
 
-cargo binstall -y cargo-edit cargo-expand cargo-outdated \
-    cargo-watch cargo-geiger cargo-chef cargo-spellcheck \
-    cargo-about cargo-valgrind
+cargo binstall  cargo-expand    --no-confirm    \
+                cargo-outdated  --no-confirm    \
+                cargo-watch     --no-confirm    \
+                cargo-geiger    --no-confirm    \
+                cargo-chef      --no-confirm    \
+                cargo-about     --no-confirm    \
+                cargo-valgrind  --no-confirm    \
+                cargo-spellcheck --no-confirm   \
+                cargo-inspect   --no-confirm    \
+                cargo-audit     --no-confirm    \
+                cargo-tarpaulin --no-confirm    \
