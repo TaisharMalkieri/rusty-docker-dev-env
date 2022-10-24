f609551d0978623833bd2c72fdf17e4f62d5f30b
tormod <tormod_tho@hotmail.com>
2022-10-24 16:27:44 +0200

    as

--- backend/build/dev/utilities.sh
+++ backend/build/dev/utilities.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 # Install curl, wget, git
+GIT_USER=${1}
+GIT_EMAIL=${2}
+
 apt-get update                  \
 && apt-get install -y git       \
 && apt-get install -y curl      \
