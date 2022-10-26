9c112b84106fedaab753cc77b3eac76ac5ae31f1
Tormod <tormod_tho@hotmail.com>
2022-10-26 11:57:57 +0000

    assa

--- backend/build/dev/shell.sh
+++ backend/build/dev/shell.sh
@@ -7,13 +7,14 @@
 export ZDOTDIR="~/dev/rusty/rusty-docker-dev-env/.config/zsh"
 
 if ! type zsh > /dev/null 2>&1; then
-    apt_get_update_if_needed
-    apt-get install -y zsh
-    apt-get update
-    chsh -s /usr/bin/zsh
+    apt_get_update_if_needed                    &&\
+    apt-get install -y zsh nano locales         &&\
+    apt-get update                              &&\
+    apt-get autoremove -y                       &&\
+    apt-get clean -y                            &&\
+    rm -r /var/cache/* /var/lib/apt/lists/*     &&\
+    chsh -s $(which zsh)
 fi
-# Install oh-my-zsh
-chsh -s $(which zsh)
 
 apt-get install fontconfig                &&\
 wget --directory-prefix="tmp/" https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip &&\
@@ -23,7 +24,14 @@ fc-cache -fv                              &&\
 rm -rf /tmp/*                             &&\
 echo "Installed font!"
 
-export STARSHIP_CONFIG="~/dev/rusty/rusty-docker-dev-env/.config/starship/starship.toml"
-export STARSHIP_CACHE=~"~/dev/rusty/rusty-docker-dev-env/.config/starship/error-cache"
+
+export STARSHIP_CONFIG="/com.docker.devenvironments.code/.config/starship/starship.toml"
+export STARSHIP_CACHE=~"/com.docker.devenvironments.code/.config/starship/error-cache/"
 
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
+
+apt-get update -y && \
+    apt-get install -y --no-install-recommends zsh zplug nano locales wget && \
+    apt-get autoremove -y && \
+    apt-get clean -y && \
+    rm -r /var/cache/* /var/lib/apt/lists/*
