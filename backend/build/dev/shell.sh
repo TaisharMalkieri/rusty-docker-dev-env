6f2ceb1bf072cd09ddab6941859af6bfe212d28b
Tormod <tormod_tho@hotmail.com>
2022-10-26 11:58:23 +0000

    asas

--- backend/build/dev/shell.sh
+++ backend/build/dev/shell.sh
@@ -8,7 +8,7 @@ export ZDOTDIR="~/dev/rusty/rusty-docker-dev-env/.config/zsh"
 
 if ! type zsh > /dev/null 2>&1; then
     apt_get_update_if_needed                    &&\
-    apt-get install -y zsh nano locales         &&\
+    apt-get install -y  --no-install-recommends zsh wget zplug nano locales fontconfig &&\
     apt-get update                              &&\
     apt-get autoremove -y                       &&\
     apt-get clean -y                            &&\
@@ -16,13 +16,37 @@ if ! type zsh > /dev/null 2>&1; then
     chsh -s $(which zsh)
 fi
 
-apt-get install fontconfig                &&\
-wget --directory-prefix="tmp/" https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip &&\
-unzip tmp/Meslo.zip -d .local/share/fonts &&\
-rm .local/share/fonts/*Windows*           &&\
+apt-get update -y && \
+    apt-get install -y --no-install-recommends zsh zplug nano locales wget && \
+    apt-get autoremove -y && \
+    apt-get clean -y && \
+    rm -r /var/cache/* /var/lib/apt/lists/*
+
+mkdir -p /usr/local/share/fonts/
+
+apt-get install fontconfig                  &&\
+wget  --directory-prefix="/tmp/" https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip &&\
+unzip -o -qq /tmp/Meslo.zip -d /usr/local/share/fonts/Meslo   &&\
+rm /usr/local/share/fonts/Meslo/*Windows*              &&\
+fc-cache -fv                                &&\
+rm -rf /tmp/*                               &&\
+echo "Installed primary font Meslo!"
+
+
+wget  --directory-prefix="/tmp/" https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/FiraMono.zip &&\
+unzip -o -qq /tmp/FiraMono.zip -d /usr/local/share/fonts/FiraMono &&\
+rm /usr/local/share/fonts/FiraMono/*Windows*            &&\
 fc-cache -fv                              &&\
 rm -rf /tmp/*                             &&\
-echo "Installed font!"
+echo "Installed backup font Fira!"
+
+
+wget  --directory-prefix="/tmp/" https://github.com/vorillaz/devicons/archive/master.zip &&\
+unzip -o -qq /tmp/master.zip -d /usr/local/share/fonts/devicons &&\
+rm /usr/local/share/fonts/devicons/*Windows*           &&\
+fc-cache -fv                              &&\
+rm -rf /tmp/*                             &&\
+echo "Installed devicons!"
 
 
 export STARSHIP_CONFIG="/com.docker.devenvironments.code/.config/starship/starship.toml"
@@ -30,8 +54,3 @@ export STARSHIP_CACHE=~"/com.docker.devenvironments.code/.config/starship/error-
 
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
 
-apt-get update -y && \
-    apt-get install -y --no-install-recommends zsh zplug nano locales wget && \
-    apt-get autoremove -y && \
-    apt-get clean -y && \
-    rm -r /var/cache/* /var/lib/apt/lists/*
--- backend/build/dev/utilities.sh
+++ backend/build/dev/utilities.sh
@@ -13,8 +13,8 @@ apt-get update                  \
 && apt upgrade -y               \
 && apt-get clean
 
-git config --global user.name "tormod"
-git config --global user.email "tormod_tho@hotmail.com"
+git config --global user.name GIT_USER
+git config --global user.email GIT_EMAIL
 
 git config --global --replace-all alias.am "!git commit --ammend --no-edit"
 git config --global --replace-all alias.save "!git add -A && git commit -m 'chore: commit save point'"
