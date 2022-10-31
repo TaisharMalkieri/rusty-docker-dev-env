a368015c1448ee0c2bbab1e7b87a6f0f7570c0de
tormod <tormod_tho@hotmail.com>
2022-10-31 15:54:37 +0100

    edited user creation

--- backend/build/dev/user.sh
+++ backend/build/dev/user.sh
@@ -12,23 +12,28 @@ if id -u "${USERNAME}" > /dev/null 2>&1; then
     # User exists, update if needed
     if [ "$USER_GID" != "$(id -g "$USERNAME")" ]; then
         # If password add password else, skip
-        if [ "$USER_GID" != "$(id -g "$USERNAME")" ]; then
-            groupmod --gid "$USER_GID" "${GROUP_NAME}"
+        groupmod --gid "$USER_GID" "$GROUP_NAME"
+        if [ "$PASSWORD" == "init" ]; then
             usermod --gid "$USER_GID" "$USERNAME"
-
+            echo "No password added"
+        else
+            usermod --gid "$USER_GID" -d "$USERNAME" -p "$PASSWORD"
+            echo "With password added"
+        fi
     fi
     if [ "$USER_UID" != "$(id -u "$USERNAME")" ]; then
         usermod --uid "$USER_UID" "$USERNAME"
     fi
 else
+    # Create user
+    groupadd --gid "$USER_GID" "$USERNAME"
     if [ "$PASSWORD" == "init" ]; then
-        # Create user
-        groupadd --gid "$USER_GID" "$USERNAME"
         useradd -s /bin/bash --uid "$USER_UID" --gid "$USER_GID" -m "$USERNAME"
+        echo "User created without password"
     else
-        # Create user
-        groupadd --gid "$USER_GID" "$USERNAME"
         useradd -s /bin/bash --uid "$USER_UID" --gid "$USER_GID" -m "$USERNAME" -p "$PASSWORD"
+        echo "User created with password"
+    fi
 fi
 
 if [ "${USERNAME}" != "root" ] && [ "${EXISTING_NON_ROOT_USER}" != "${USERNAME}" ]; then
