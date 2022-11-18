cf932410f2bd85d16a1753e1f1d34c51359b47a5
Tormod <tormod_tho@hotmail.com>
2022-11-18 13:23:21 +0100

    add default to rustup and cargo-home

--- backend/build/dev/rust.sh
+++ backend/build/dev/rust.sh
@@ -3,6 +3,10 @@
 RUST_VERSION=${1:-"stable"}
 RUSTUP_INIT_VERSION=${2:-"1.24.3"}
 RUST_ANALYZER_VERSION=${3:-"2022-08-22"}
+RUSTUP_HOME=${4:-"/usr/local/rustup"}
+CARGO_HOME=${5:-"/usr/local/cargo"}
+
+
 
 PATH=/usr/local/cargo/bin:$PATH
 
@@ -16,7 +20,7 @@ aarch64) rustArch='aarch64-unknown-linux-gnu'; rustupSha256='32a1532f7cef072a667
 esac
 
 echo "Installing rust-$RUST_VERSION, rustup-$RUSTUP_INIT_VERSION, rust-analyzer $RUST_ANALYZER_VERSION for $arch"
-
+export RUSTUP_HOME
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs | bash -s -- -y \
     --no-modify-path                        \
     --profile minimal                       \
@@ -24,14 +28,30 @@ curl --proto '=https' --tlsv1.2 https://sh.rustup.rs | bash -s -- -y \
     --default-host "$rustArch"             \
     --component add clippy rustfmt rust-src
 
-apt-get update
-apt-get -y install --no-install-recommends gcc libc6-dev musl-tools
-apt-get clean
-ln -s /usr/bin/gcc /usr/bin/"$(uname -m)"-linux-musl-gcc
-chmod -R a+w "${RUSTUP_HOME}" "${CARGO_HOME}"
-
+export CARGO_HOME
+apt-get update && \
+apt-get -y install --no-install-recommends gcc libc6-dev musl-tools && \
+apt-get clean && \
+ln -s /usr/bin/gcc /usr/bin/"$(uname -m)"-linux-musl-gcc && \
+chmod -R a+w "${RUSTUP_HOME}" "${CARGO_HOME}" && \
 rustup default stable
 
-wget -qO- "https://github.com/rust-analyzer/rust-analyzer/releases/download/$RUST_ANALYZER_VERSION/rust-analyzer-$(uname -m)-unknown-linux-gnu.gz" | \
+wget -qO- "https://github.com/rust-analyzer/rust-analyzer/releases/download/$RUST_ANALYZER_VERSION/rust-analyzer-$(uname -m)-unknown-linux-gnu.gz" |
 gunzip > /usr/local/bin/rust-analyzer && \
 chmod 500 /usr/local/bin/rust-analyzer
+
+# Install dev-ops packages
+cargo install cargo-binstall
+cargo install --locked cargo-chef
+cargo binstall cargo-binstall --no-confirm
+
+cargo binstall  --no-confirm        \
+                cargo-expand        \
+                cargo-outdated      \
+                cargo-watch         \
+                cargo-geiger        \
+                cargo-about         \
+                cargo-spellcheck    \
+                cargo-inspect       \
+                cargo-audit         \
+                cargo-clippy        \
