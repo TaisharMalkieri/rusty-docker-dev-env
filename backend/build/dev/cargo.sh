7458af50d6fcbcf2c4e3030bf0ec3aec1cd81366
Tormod <tormod_tho@hotmail.com>
2022-11-18 13:29:58 +0100

    CORR

--- backend/build/dev/cargo.sh
+++ backend/build/dev/cargo.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
-rustup default stable
 
-cargo install cargo-binstall
-cargo install --locked cargo-chef
-cargo binstall cargo-binstall --no-confirm
 
-cargo binstall  --no-confirm        \
-                cargo-expand        \
-                cargo-outdated      \
-                cargo-watch         \
-                cargo-geiger        \
-                cargo-about         \
-                cargo-spellcheck    \
-                cargo-inspect       \
-                cargo-audit         \
-                cargo-clippy        \
+
 #                cargo-tarpaulin
 #                cargo-valgrind
